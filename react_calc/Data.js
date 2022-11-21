22b48dd0fedb9356faa1885fc520aaaa360519e9
Ber1itz <97990880+Ber1itz@users.noreply.github.com>
2022-11-21 00:43:45 -0500

    Add files via upload

---
+++ react_calc/Data.js
@@ -0,0 +1,71 @@
+const Data = [
+    {
+        symbol: "/",
+        className: "input btn btn-secondary text-dark fw-bold fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: "%",
+        className: "input btn btn-warning text-dark fs-5 ms-1 mt-2",
+    },
+    {
+        symbol: "7",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: "8",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: "9",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: "*",
+        className: "input btn btn-warning text-dark fs-5 p2-3 ms-1 mt-2",
+    },
+    {
+        symbol: "4",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: "5",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: "6",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: "-",
+        className: "input btn btn-warning text-dark fs-5 p2-3 ms-1 mt-2",
+    },
+    {
+        symbol: "1",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: "2",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: "3",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: "+",
+        className: "input btn btn-warning text-dark fs-5 p2-3 ms-1 mt-2",
+    },
+    {
+        symbol: "+/-",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: "0",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+    {
+        symbol: ".",
+        className: "input btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+    },
+];
+export default Data;
